947898866a6b52ef4d0d483e591b50cb1629d578
Paul Bell <paubel@ec-kitslab-0.maxlab.lu.se>
2014-06-02 13:14:30 +0200

    add dir for json files

--- dsconfig/excel_alarms_vac.py
+++ dsconfig/excel_alarms_vac.py
@@ -17,8 +17,8 @@ class SuperDict(defaultdict):
         return self[attr]
 
 
-def add_device(sdict, inst, dev, al_name, al_cond, al_desc):
-    print inst, dev, al_name, al_cond, al_desc
+def add_device(sdict, inst, dev, al_name, al_cond, al_desc, al_sev):
+    print inst, dev, al_name, al_cond, al_desc, al_sev
     # devdict = sdict.servers["PyAlarm"]["PyAlarm/"+inst]["PyAlarm"][dev]
     devdict = sdict.servers["PyAlarm/"+inst]["PyAlarm"][dev]
     if "AlarmList" not in devdict.properties:
@@ -30,7 +30,7 @@ def add_device(sdict, inst, dev, al_name, al_cond, al_desc):
     #hard code severity and some other things - only one per instance
     if "AlarmSeverities" not in devdict.properties:
         devdict.properties["AlarmSeverities"] = []
-    devdict.properties["AlarmSeverities"].append(al_name+":ALARM")
+    devdict.properties["AlarmSeverities"].append(al_name+":"+al_sev)
     if "AlarmReceivers" not in devdict.properties:
         devdict.properties["AlarmReceivers"] = []
     devdict.properties["AlarmReceivers"].append(al_name+":HTML")
@@ -46,7 +46,7 @@ def add_device(sdict, inst, dev, al_name, al_cond, al_desc):
     devdict.properties["HtmlFolder"] = ["/tmp/pjb"]
     if "PollingPeriod" not in devdict.properties:
         devdict.properties["PollingPeriod"] = []
-    devdict.properties["PollingPeriod"] = [3]
+    devdict.properties["PollingPeriod"] = [5]
     if "MaxMessagesPerAlarm" not in devdict.properties:
         devdict.properties["MaxMessagesPerAlarm"] = []
     devdict.properties["MaxMessagesPerAlarm"]= [1]
@@ -60,36 +60,47 @@ def add_device(sdict, inst, dev, al_name, al_cond, al_desc):
 def xls_to_dict(xls_filename):
     json_dict = SuperDict()
     xls = xlrd.open_workbook(xls_filename)
-    sheet = xls.sheet_by_name("Alarms")
-    last_server=""
-    last_device=""
-    last_name=""
-    summary_condition=""
-    for line in xrange(1, sheet.nrows):
-        # above skips row 0 (col headers)
-        # look at all rows but only read those with entry in first col
-        if sheet.row_values(line)[0] is not "":
-            print "IN LINE ", line, sheet.row_values(line)[0] 
+
+    for i in range (0,2):
+
+        if i==1:
+            sheet = xls.sheet_by_name("Alarms")            
+            nature="interlock"
+        else:
+            sheet = xls.sheet_by_name("Warnings")            
+            nature="bypass"
+
+        last_server=""
+        last_device=""
+        last_name=""
+        last_sev=""
+        summary_condition=""
+        for line in xrange(1, sheet.nrows):
+            # above skips row 0 (col headers)
+            # look at all rows but only read those with entry in first col
+            if sheet.row_values(line)[0] is not "":
+                print "IN LINE ", line, sheet.row_values(line)[0] 
             #assume that if you get to a new device, it means a new section of vacuum
             #in this case, need to make a final alarm which is or of all others
-            dev_config = sheet.row_values(line)
-            print dev_config, dev_config[3].rsplit("/",1)[0] 
-            if dev_config[1] != last_device or dev_config[0]=="end":
-                print "START NEW SECTION", dev_config[1]
-                print "---- ADDING TO JSON summary ", summary_condition, last_name
-                if summary_condition!="":
-                    add_device(json_dict,last_server,last_device,last_name.rsplit("_",1)[0],summary_condition,"at least one vac. interlock in section %s" %last_name.rsplit("_",2)[0])
-                last_server = dev_config[0]
-                last_device = dev_config[1]
-                last_name = dev_config[2]
-                summary_condition=""
-            if summary_condition == "":
-                summary_condition = summary_condition + dev_config[3]
-            else:
-                summary_condition = summary_condition + " or " + dev_config[3]
+                dev_config = sheet.row_values(line)
+                print dev_config, dev_config[3].rsplit("/",1)[0] 
+                if dev_config[1] != last_device or dev_config[0]=="end":
+                    print "START NEW SECTION", dev_config[1]
+                    print "---- ADDING TO JSON summary ", summary_condition, last_name
+                    if summary_condition!="":
+                        add_device(json_dict,last_server,last_device,last_name.rsplit("_",1)[0],summary_condition,"at least one vac. %s in section %s" %(nature,last_name.rsplit("__",2)[0]),last_sev)
+                    last_server = dev_config[0]
+                    last_device = dev_config[1]
+                    last_name = dev_config[2]
+                    last_sev = dev_config[5]
+                    summary_condition=""
+                if summary_condition == "":
+                    summary_condition = summary_condition + dev_config[3]
+                else:
+                    summary_condition = summary_condition + " or " + dev_config[3]
 
-            if dev_config[0]!="end":
-                add_device(json_dict, *dev_config[:5])
+                if dev_config[0]!="end":
+                    add_device(json_dict, *dev_config[:6])
 
     return json_dict
 
