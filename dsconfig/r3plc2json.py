9cbf3ad943ff5f9678ac2942dd24e82b17ea71f6
Paul Bell <paul.bell@maxlab.lu.se>
2014-12-05 16:00:15 +0100

    ring plc script

--- dsconfig/r3plc2json.py
+++ dsconfig/r3plc2json.py
@@ -73,8 +73,8 @@ def make_pyalarm_properties(etherip_dev, origrow, alarms_rows):
 
     prop_dict = AppendingDict()
 
-    #do something for pyalarm here?
     #look for individual alma and almd tags in other sheet
+
     added = []
     column_names = alarms_rows[0]
     for i, row_ in enumerate(alarms_rows[1:]):
@@ -119,8 +119,6 @@ def make_eth_ip_alarm_properties(origrow, alarms_rows):
 
     prop_dict = AppendingDict()
 
-    
-    #do something for pyalarm here?
     #look for individual alma/almd tags in other sheet
     added = []
     column_names = alarms_rows[0]
@@ -142,12 +140,16 @@ def make_eth_ip_alarm_properties(origrow, alarms_rows):
 
 
     return prop_dict
-  
-###############################################################################################
 
-def make_db_name(name):
-    "convert a Space Separated Name into a lowercase, underscore_separated_name"
-    return name.strip().lower().replace(" ", "_")
+###############################################################################################
+def process_achromat_col_name(name):
+    if str(name)=="R3" or str(name[0])=="B":
+        newname = str(name)
+    elif "PLC0" in str(name): #put tags related to plc itself under r3
+        newname = "R3"
+    else:
+        newname = "R3-"+str(name)
+    return newname
 
 ###############################################################################################
 
@@ -155,25 +157,14 @@ def make_eth_ip_device(plcnum,achromat,plcsystem):
     """
     Return ether ip device name
     """
-    #EtherIP device is like R-311/VAC/PLC-01
-    #The achromat changes and PLC can be 01 or 02, this is always VAC subsystem
-    if str(achromat)=="R3":
-        domain = achromat
-    else:
-        domain = "R3-"+achromat
+    #EtherIP device is typically like R-311/VAC/PLC-01
+    #In the spreadsheet, achromat may be "R3" for something general, 3xx for an achromat
+    #or Bxxx for a beamline
+    #domain = process_achromat_col_name(achromat)
+    domain = achromat
     family = plcsystem
     member = "PLC-0%i"%plcnum
-    
     return domain+"/"+family+"/"+member
-
-###############################################################################################
-
-def make_eth_ip_server(achromat):
-    if str(achromat)=="R3":
-        inst = achromat
-    else:
-        inst = "R3-"+achromat
-    return "%s/%s" % ("AllenBradleyEIP", inst)
 
 ###############################################################################################
 
@@ -181,21 +172,26 @@ def make_pyalarm_device(plcnum,achromat,plcsystem):
     """
     Return pyalarm device name
     """
-    #PyAlarm device is like R-311/VAC/PLC-01-ALARM
+    #PyAlarm device is typically like R-311/VAC/PLC-01-ALARM
     #The achromat changes and PLC can be 01 or 02, this is always VAC subsystem
-    domain = "R3-"+achromat
+    #domain = process_achromat_col_name(achromat)
+    domain = achromat
     family = plcsystem
     member = "PLC-0%i-ALARM"%plcnum
-    
     return domain+"/"+family+"/"+member
 
 ###############################################################################################
 
+def make_eth_ip_server(achromat):
+    #inst = process_achromat_col_name(achromat)
+    inst = achromat
+    return "%s/%s" % ("AllenBradleyEIP", inst)
+
+###############################################################################################
+
 def make_pyalarm_server(achromat):
-    if str(achromat)=="R3":
-        inst = achromat
-    else:
-        inst = "R3-"+achromat 
+    #inst = process_achromat_col_name(achromat)
+    inst = achromat
     return "%s/%s" % ("PyAlarm", inst)
 
 ###############################################################################################
@@ -207,10 +203,7 @@ def make_py_att_device(achromat,subsystem,number):
     
     #PyAttProc device is like R-311/VAC/TC0-01
     #get "311", "WAT" and "TCO06"
-    if achromat=="R3":
-        domain = achromat
-    else:
-        domain = "R3-"+achromat
+    domain = process_achromat_col_name(achromat)
     family = subsystem
     member = number[:-2] + '-' + number[-2:]
     
@@ -269,15 +262,19 @@ def convert(plc_config, plctype, plcnum,rows, alarms_rows, definitions, skip=Tru
             already_added_tags.append(row["Tag"])   
 
             achromat = row["Achromat"]
-            #xxxpjbpy_att_device = make_py_att_device(row["Achromat"],row["Subsystem"],row["No"])
+
+            #rename what is in first column. e.g. add R3 prefix if needed
+            #special case for plc01 and 02, get merged into r3
+            achromat = process_achromat_col_name(achromat)
+
+            #py_att_device = make_py_att_device(row["Achromat"],row["Subsystem"],row["No"])
             eth_ip_device = make_eth_ip_device(plcnum,achromat,plctype)
+
             try:
                 # full device definition
-                #xxpjbpyatt_srvr = make_py_att_server(row["Subsystem"])
+                #pyatt_srvr = make_py_att_server(row["Subsystem"])
                 ethip_srvr = make_eth_ip_server(achromat)
-                # target is "lazily" evaluated, so that we don't create
-                # an empty dict if it turns out there are no members
-                #xxxpjbpyatt_target = lambda: definitions.servers[pyatt_srvr]["PyAttributeProcessor"][py_att_device]
+                #pyatt_target = lambda: definitions.servers[pyatt_srvr]["PyAttributeProcessor"][py_att_device]
                 ethip_target = lambda: definitions.servers[ethip_srvr]["AllenBradleyEIP"][eth_ip_device]
 
                 #if type is alma there must be alarm tags in other sheet
@@ -294,22 +291,18 @@ def convert(plc_config, plctype, plcnum,rows, alarms_rows, definitions, skip=Tru
         else:  
             print "ROW NOT PARSED", row
 
-
-
         #put some properties into py att device - everything but alarms
         ###py_att_props = make_py_att_properties(row)
         ###if py_att_props:
         ###    pyatt_target().properties = py_att_props
 
-        #put tags as properties into ether ip device (skip alma ones)
+        #put tags as properties into ether ip device (skip alma and almd ones)
         if not is_almad:
             eth_ip_props = make_eth_ip_properties(row)
 
         #put alarm tags from other sheet as properties into ether ip device
         if is_almad:
-
             eth_ip_alarm_props = make_eth_ip_alarm_properties(row, alarms_rows)
-
             #set pyalarm props, too
             pyalarm_props = make_pyalarm_properties(eth_ip_device, row, alarms_rows)
 
@@ -317,15 +310,15 @@ def convert(plc_config, plctype, plcnum,rows, alarms_rows, definitions, skip=Tru
         if not is_almad and eth_ip_props:
             ethip_target().properties = eth_ip_props
 
-            #add one-off properties. This assumes one etherip device for one achromat, not very flexible!
+            #add one-off properties. This assumes one etherip device for one achromat! (or beamline)
+            #achromat here is as written in the sheet, not what becomes domaine or instance
             if achromat not in already_added_ethip:
                 already_added_ethip.append(achromat)
                 single_prop_dict = {} #not appending!
                 single_prop_dict["MinimumScanPeriod"] = plc_config["minscan"]
-                single_prop_dict["CPUSlot"] = plc_config["slot"]
+                single_prop_dict["CPUSlot"] = int(plc_config["slot"])
                 single_prop_dict["PLC"] = plc_config["ip"]
                 ethip_target().properties = single_prop_dict
-
 
 
         if is_almad and eth_ip_alarm_props:
@@ -339,11 +332,11 @@ def convert(plc_config, plctype, plcnum,rows, alarms_rows, definitions, skip=Tru
                 already_added_ethip.append(achromat)
                 single_prop_dict = {} #not appending!
                 single_prop_dict["MinimumScanPeriod"] = plc_config["minscan"]
-                single_prop_dict["CPUSlot"] = plc_config["slot"]
+                single_prop_dict["CPUSlot"] = int(plc_config["slot"])
                 single_prop_dict["PLC"] = plc_config["ip"]
                 ethip_target().properties = single_prop_dict
 
-            #add one-off properties. This assumes one alarm device for one achromat, not very flexible!
+            #add one-off properties. This assumes one alarm device for one achromat!
             if achromat not in already_added_pyala:
                 already_added_pyala.append(achromat)
                 single_prop_dict = {}
@@ -395,7 +388,7 @@ def xls_to_dict(xls_filename, pages=None, skip=False):
     print " configs ", plc1_config, plc2_config
 
     for page in pages:
-        print "in page ", page
+        print "In page ", page
         if page == "Config":
             continue #we already read this one!
 
