020e0063347812b6a3905b89a3241cf430bca5bd
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-02-22 17:03:12 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@
 from setuptools import setup
 
 setup(
+    # Package
     name="python-dsconfig",
     version="1.0.0",
-    install_requires=['jsonpatch>=1.13'],
-    description="Library and utilities for Tango device configuration.",
     packages=['dsconfig', 'dsconfig.appending_dict'],
-    test_suite="nose.collector",
-    package_data={'dsconfig': ['schema/dsconfig.json', 'schema/schema2.json']},
+    description="Library and utilities for Tango device configuration.",
+    # Requirements
+    install_requires=['jsonpatch>=1.13'],
+    setup_requires=['pytest-runner'],
+    tests_require=["pytest", "pytest-cov", "fake-factory", "mock"],
+    # Resources
+    package_data={
+        'dsconfig': ['schema/dsconfig.json',
+                     'schema/schema2.json']},
+    # Scripts
     entry_points={
         'console_scripts': ['xls2json = dsconfig.excel:main',
                             'csv2json = dsconfig.callcsv:main',
