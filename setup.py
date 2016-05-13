d7f9cad3892c8894d1646554356fba89a4e22b38
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2016-05-13 13:52:49 +0200

    bump v 1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     # Package
     name="python-dsconfig",
-    version="1.0.3",
+    version="1.0.4",
     packages=['dsconfig', 'dsconfig.appending_dict'],
     description="Library and utilities for Tango device configuration.",
     # Requirements
