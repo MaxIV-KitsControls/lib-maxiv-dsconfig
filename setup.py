6245006a63163b8a6d556b242bb3d25c654502e7
Wojciech Kitka <wojciech.kitka@s2innovation.com>
2020-06-04 09:12:13 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     # Package
     name="python-dsconfig",
-    version="1.4.0",
+    version="1.5.0",
     packages=["dsconfig", "dsconfig.appending_dict"],
     description="Library and utilities for Tango device configuration.",
     # Requirements
